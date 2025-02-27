4943c1cb97e3a89c761b08f90c51afeb76a6d253
gittymo <mooglestuff@gmail.com>
2025-02-27 21:50:59 +0000

    Made logger thread safe.

---
+++ TestSimpleLogger/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleLogger;
+
+Logger logger = new Logger("log.txt", 5);
+logger.Log("INFO", "Hello, {0}!", "World");
+
+Console.WriteLine("Hello, World!");
